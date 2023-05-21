e8776ed0c8916d1d93d39e7b9d371bae246a6488
armantovmasyan <arman-nn04@mail.ru>
2023-05-22 01:49:05 +0300

    Unignored links.py

---
+++ links.py
@@ -0,0 +1,5 @@
+google_sheet = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSo-q5e1DCc6-TpwxETCuL2L2_W6EHTITHb00-JyhvPUmLjuIuSXj3exCINfcI0wU5OE5AFzYleaPdK/pubhtml?gid=2006076710&single=true"
+links = ["https://www.kgeorgiy.info/upload/paradigms/linux/table.html",
+         "https://www.kgeorgiy.info/upload/paradigms/windows/table.html",
+         "https://www.kgeorgiy.info/upload/paradigms/macos/table.html"]
+systems = ["Linux", "Windows", "MacOS"]
